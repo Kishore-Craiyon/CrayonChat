7913b2aea141ed471a3b7721f6edacbc260c80dd
Kishore <kishoresmile08@gmail.com.com>
2025-09-14 00:37:25 +0530

    Signup Updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,10 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express(); 
 
-
 const PORT = process.env.PORT;
+
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, ()=>{
